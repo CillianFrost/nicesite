a63eb6b5f01df928a522e6eba238b122b32a0605
Sergey <coolwindblows@protonmail.com>
2018-07-07 23:12:12 +0500

    header and table fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,51 +17,44 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header_nav" aria-controls="header_nav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <ul class="navbar-nav">
-                    @foreach($pages as $page)
-                        <li class="nav-item 
-                            @if(Route::currentRouteName() == $page->slug && Route::currentRouteName() !== 'store') 
-                                active
-                            @endif 
-                            @if($page->slug == 'store') 
-                                mr-2 
-                            @endif">
-                            <a class="nav-link" href="{{ route($page->slug) }}">
-                                {{ $page->title }}
+                <div class="collapse navbar-collapse" id="header_nav">
+                    <ul class="navbar-nav align-items-center">
+                        @foreach($pages as $page)
+                            <li class="nav-item @if(Route::currentRouteName() == $page->slug && Route::currentRouteName() !== 'store') active @endif @if($page->slug == 'store') mr-2 @endif">
+                                <a class="nav-link" href="{{ route($page->slug) }}">{{ $page->title }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown mr-5">
+                            <i class="fa fa-bell dropdown-toggle" aria-hidden="true" data-toggle="dropdown"></i>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">
+                                Link 1
                             </a>
+                            <a class="dropdown-item" href="#">
+                                Link 2
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Link 3
+                            </a>
+                        </div>
                         </li>
-                    @endforeach
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item dropdown mr-5">
-                        <i class="fa fa-bell dropdown-toggle" aria-hidden="true" data-toggle="dropdown"></i>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">
-                            Link 1
-                        </a>
-                        <a class="dropdown-item" href="#">
-                            Link 2
-                        </a>
-                        <a class="dropdown-item" href="#">
-                            Link 3
-                        </a>
-                    </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <i class="fa fa-user dropdown-toggle" aria-hidden="true" data-toggle="dropdown"></i>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">
-                            Link 1
-                        </a>
-                        <a class="dropdown-item" href="#">
-                            Link 2
-                        </a>
-                        <a class="dropdown-item" href="#">
-                            Link 3
-                        </a>
-                    </div>
-                    </li>
-                </ul>
+                        <li class="nav-item dropdown">
+                            <i class="fa fa-user dropdown-toggle" aria-hidden="true" data-toggle="dropdown"></i>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">
+                                Link 1
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Link 2
+                            </a>
+                            <a class="dropdown-item" href="#">
+                                Link 3
+                            </a>
+                        </div>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </header>
@@ -69,13 +62,9 @@
             @yield('content')
         </main>
         <footer class="nice_padding container-fluid mb-3">
-            <span class="mr-2">
-                &#9400; 2015
-            </span>
+            <span class="mr-2">&#9400; 2015</span>
             <span class="nice_link">
-                <a href="https://www.interesnee.ru/" target="_blank">
-                    interesnee.ru
-                </a>
+                <a href="https://www.interesnee.ru/" target="_blank">interesnee.ru</a>
             </span>
         </footer>
         <script src="{{ asset('js/app.js') }}">  </script>
